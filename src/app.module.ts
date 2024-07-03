13371e26c2f981c36597eab1fb00d8f4c35157ee
totani <norihiro.totani@arsaga.jp>
2024-07-03 22:54:29 +0900

    データベースの設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import  configurations  from './config/configuration'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({load: configurations}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
+        entities: ['dist/**/entities/*.entity.js']
+      }),
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
