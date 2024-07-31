eb2ce90de6031023132ca5dfac880fadf1f0bc01
totani <norihiro.totani@arsaga.jp>
2024-07-31 09:51:32 +0900

    dockerでの環境構築修正と、typeormの設定修正

---
+++ typeOrm.config.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm';
+
+export default new DataSource({
+  type: 'mysql',
+  host: 'mysql',
+  port: 3306,
+  database: 'question',
+  username: 'user',
+  password: 'password',
+  entities: ['dist/**/entities/**/*.entity.js'],
+  migrations: ['dist/**/migrations/**/*.js'],
+  logging: true,
+});
